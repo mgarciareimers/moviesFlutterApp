2b3039a604eed2c3973a42a55ce007e1b855d7cb
Miguel García <miguel.garcia@magocode.com>
2019-11-18 00:51:44 +0100

    Start of Project.

---
+++ movies/lib/src/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:movies/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movies',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/' : (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
